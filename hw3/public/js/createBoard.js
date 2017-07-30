43d2c0fcb53240a1d9f5f9cdaa93f9b5f92b2694
Andrew Vu <anv012@ucsd.edu>
2017-07-30 02:43:27 -0700

    toggle views/themes/contentChange/Json Objects

--- hw3/public/js/createBoard.js
+++ hw3/public/js/createBoard.js
@@ -5,35 +5,30 @@
     };
 */
 
-var sb = {
-   size : 12,
-   image: ["images/audiosound.png","images/audiosound.png", "images/audiosound.png",
-            "images/audiosound.png","images/audiosound.png", "images/audiosound.png",
-            "images/audiosound.png","images/audiosound.png", "images/audiosound.png",
-            "images/audiosound.png","images/audiosound.png", "images/audiosound.png"],
-   sound: ["audio/applause.wav", "audio/bassc2.wav", "audio/bassc3.wav",
-            "audio/bassloop.wav", "audio/boom.wav", "audio/contrac2.wav",
-            "audio/fatsynslap.wav", "audio/guitarc4.wav", "audio/hihat.wav",
-            "audio/hornc5.wav", "audio/pianoc6.wav", "audio/snare.wav"],
-   type:["audio/wav", "audio/wav", "audio/wav", "audio/wav",
-        "audio/wav", "audio/wav", "audio/wav", "audio/wav",
-         "audio/wav", "audio/wav", "audio/wav", "audio/wav"]
- 
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send(); 
 }
 
-var cats = {
-    size : 12,
-    image: ["images/cat1.png","images/cat2.png", "images/cat3.jpg", "images/cat4.png",
-            "images/cat1.png","images/cat2.png", "images/cat3.jpg", "images/cat4.png",
-            "images/cat1.png","images/cat2.png", "images/cat3.jpg", "images/cat4.png"],
-    sound: ["audio/cat1.wav", "audio/cat2.wav","audio/cat3.wav",
-            "audio/cat4.wav", "audio/cat5.wav", "audio/cat6.wav",
-            "audio/cat7.wav", "audio/cat8.wav","audio/cat9.wav",
-            "audio/cat10.wav", "audio/cat11.wav", "audio/cat12.wav"],
-    type:["audio/wav", "audio/wav", "audio/wav", "audio/wav",
-        "audio/wav", "audio/wav", "audio/wav", "audio/wav",
-         "audio/wav", "audio/wav", "audio/wav", "audio/wav"]
-}
+// this requests the file and executes a callback with the parsed result once
+//   it is available
+
+var instr = null;
+var cats = null;
+fetchJSONFile('../sb.json', function(data){instr = data;});
+fetchJSONFile('../cats.json', function(data){cats = data;});
+
+
+
 
 function addThemes(){
 
@@ -89,12 +84,9 @@ function createImageBoard(){
             var sound = document.getElementById(audio_id);
             document.getElementById("testing").innerHTML +=audio_id;
             if (sound.paused) {
-                this.parentElement.style.border="thick solid green";
                 sound.play();
-                this.parentElement.style.border="thick solid transparent";
             } else { 
                 sound.pause();
-                this.parentElement.style.border="thick solid transparent";
 		    }
 
         }, false);
@@ -106,26 +98,34 @@ function createImageList(){
 
     for(i=0; i<12;i++){
         var list_id = 'l'+i;
-        sb_list.innerHTML+='<li> <img id="'+list_id+'" src="'+sb.image[i]+'"></li>';
-        
-
+        var sound_name = sb.sound[i].replace("audio/", "");
+        sb_list.innerHTML+='<li> <a id="'+list_id+'" href="#" onclick="soundAction('+i+')">'+sound_name+'</a>';
+       
         document.getElementById(list_id).addEventListener('click', function(){
             var img_id = this.id;
             var audio_id = 'a' + img_id.replace(/[^0-9]/g, ''); 
             var sound = document.getElementById(audio_id);
             document.getElementById("testing").innerHTML +=audio_id;
             if (sound.paused) {
-                this.parentElement.style.border="thick solid green";
                 sound.play();
-                this.parentElement.style.border="thick solid transparent";
             } else { 
                 sound.pause();
-                this.parentElement.style.border="thick solid transparent";
 		    }
 
         }, false);
     }
     sb_list.innerHTML+= "</ul>"
+}
+
+function soundAction(num){
+    var audio_id = 'a' + num;
+    var sound = document.getElementById(audio_id);
+    document.getElementById("testing").innerHTML +=audio_id;
+    if (sound.paused) {
+        sound.play();
+    } else { 
+        sound.pause();
+    }
 }
 function changeSoundBoard(jsObject){
     var sb= jsObject;
@@ -134,18 +134,35 @@ function changeSoundBoard(jsObject){
         var new_img = sb.image[i];
         var new_audio = sb.sound[i];
         var new_audio_type=sb.type[i];
+        var new_audio_name = sb.sound[i].replace("audio/", "");
         var old_id= 'i'+i;
         var old_lid='l'+i;
         var old_aid='a'+i;
 
         var old_img = document.getElementById(old_id);
-        var old_limg = document.getElementById(old_lid);
+        var old_audio_list = document.getElementById(old_lid);
         var old_audio = document.getElementById(old_aid);
         
         old_img.src = new_img;
         old_audio.src = new_audio;
-        old_limg.src = new_img;
+        old_audio_list.innerHTML = new_audio_name;
         old_audio.type = new_audio_type;
+    }
+}
+
+function toggleBoard(){
+    var sb_list = document.getElementById("sb_list");
+    var sb_grid = document.getElementById("sb_grid");
+    var btn = document.getElementById("togglebtn");
+    if(sb_list.style.display == "none"){
+        sb_list.style.display="block";
+        sb_grid.style.display="none";
+        btn.innerHTML = "Toggle To Icon View";
+    }
+    else{
+        sb_list.style.display="none";
+        sb_grid.style.display="block";
+        btn.innerHTML= "Toggle To List View";
     }
 }
 
